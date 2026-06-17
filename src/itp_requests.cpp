2ec147d456203283b553e9745083612838316226
Sam <467704+samphonic@users.noreply.github.com>
2026-06-17 18:03:36 +0000

    Print hex for packet type

--- src/itp_requests.cpp
+++ src/itp_requests.cpp
@@ -31,7 +31,7 @@ std::optional<RawPacket> ITPPacketReader::check_for_packet() {
     byte_provider_.read_array(&packet_buffer_[PACKET_HEADER_SIZE],
                               packet_buffer_[PACKET_HEADER_INDEX_PAYLOAD_LENGTH] + 1);
     auto rp = RawPacket(packet_buffer_, PACKET_HEADER_SIZE + packet_buffer_[PACKET_HEADER_INDEX_PAYLOAD_LENGTH] + 1);
-    ITP_LOGD(REQUESTS_TAG, "Received %i packet on %s.", rp.get_packet_type(), log_name_);
+    ITP_LOGV(REQUESTS_TAG, "Received %x packet on %s.", rp.get_packet_type(), log_name_);
 
     if (rp.is_checksum_valid()) {
       buffer_position_ = 0;  // Reset buffer
