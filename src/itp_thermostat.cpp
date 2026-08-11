454c5232aa3bff2e850ec3d42ba12e7e64ae05ba
Sam <467704+samphonic@users.noreply.github.com>
2026-08-11 06:32:36 +0000

    Remove additional debug logging

--- src/itp_thermostat.cpp
+++ src/itp_thermostat.cpp
@@ -33,13 +33,10 @@ Task Thermostat::handle_thermostat_request(RawPacket &raw_request_packet) {
         case GetCommand::SETTINGS:
           // If enhanced_mhk_ && auto_mode_ , we need to tell the MHK2 that we're in "auto" mode, despite the actual
           // mode
-          ITP_LOGW(THERMOSTAT_TAG, "Settings Get: %b %b", enhanced_mhk_, auto_mode_);
           if (enhanced_mhk_ && auto_mode_) {
-            ITP_LOGV(THERMOSTAT_TAG, "Modifying packet!");
             return send_to_heatpump<GetRequestPacket, SettingsGetResponsePacket>(
                 raw_request_packet, [this](SettingsGetResponsePacket &response_pkt) {
                   response_pkt.set_mode(SettingsSetRequestPacket::MODE_BYTE_AUTO);
-                  ITP_LOGV(THERMOSTAT_TAG, "Set mode byte to %x", response_pkt.get_mode());
                 });
           }
           return send_to_heatpump<GetRequestPacket, SettingsGetResponsePacket>(raw_request_packet);
